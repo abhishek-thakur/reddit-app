1a603debaed9967ab89800f2c39fd40bafc967be
Abhishek Thakur <th.abhishek12@gmail.com>
2021-09-16 22:03:03 +0530

    reddit-app pushed to github

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PostTitle>  
+    </PostTitle>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostTitle from './components/PostTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PostTitle
   }
 }
 </script>
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
 }
 </style>
---
+++ src/components/PostTitle.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <!-- <button type="button" class="btn btn-primary" @click.once="showPosts">Primary</button> -->
+    <ol class="list-group list-group-numbered">
+      <li v-for="title in titles" v-bind:key="title" class="list-group-item list-group-item-action fs-6">{{title}}</li>
+    </ol>
+  </div>
+  
+</template>
+
+<script>
+//import Vue from 'vue';
+import snoowrap from 'snoowrap';
+const r = new snoowrap({
+  userAgent: 'abhi-app',
+    clientId: 'GfghFfcmKGmnUQDbFxeWRA',
+    clientSecret: 'dkuYfDEqzKY-mgDQjuwYju18ibc7cg',
+    refreshToken: '1140091207469-uK3jSA-mUVl0cTgiiQahB56PMj7T-A'
+
+})
+export default {
+  name: 'PostTitle',
+  data () {
+    return {
+      titles:""
+    };
+  },
+  created(){
+    r.getHot().map(post => post.title)
+      .then(titleOfPost=>{
+        return this.titles = titleOfPost
+      })
+  }
+  // methods: {
+    
+  //   showPosts () {
+  //     r.getHot().map(post => post.title)
+  //     .then(titleOfPost=>{
+  //       return this.titles = titleOfPost;
+  //     });
+      
+  //   } 
+  // }
+}
+</script>
+<style>
+li{
+  text-align: left !important;
+  padding: 5px !important;
+  margin-left:10px;
+}
+</style>
