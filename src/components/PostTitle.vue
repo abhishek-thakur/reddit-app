a355d40aa4233d4f84b8bad6d540e475cd063f4b
Abhishek Thakur <th.abhishek12@gmail.com>
2021-09-17 18:00:14 +0530

    Update PostTitle.vue

--- src/components/PostTitle.vue
+++ src/components/PostTitle.vue
@@ -2,7 +2,11 @@
   <div>
     <!-- <button type="button" class="btn btn-primary" @click.once="showPosts">Primary</button> -->
     <ol class="list-group list-group-numbered">
-      <li v-for="title in titles" v-bind:key="title" class="list-group-item list-group-item-action fs-6">{{title}}</li>
+      <li v-for="(title, index) in titles" :key="title" class="list-group-item list-group-item-action fs-6">{{title}} 
+        <span class="bg-info"> Sub-Reddit@: {{subreddit[index]}}</span>
+        <span class="bg-warning"> Author name:{{authors[index]}}</span>
+      </li>
+      
     </ol>
   </div>
   
@@ -15,13 +19,23 @@ export default {
   name: 'PostTitle',
   data () {
     return {
-      titles:""
+      titles:"",
+      authors:"",
+      subreddit:""
     };
   },
   created(){
     snoo.getHot().map(post => post.title)
       .then(titleOfPost=>{
-        return this.titles = titleOfPost
+        return this.titles = titleOfPost  
+      });
+      snoo.getHot().map(post => post.author.name)
+      .then(authorOfPost =>{
+        return this.authors = authorOfPost
+      });
+      snoo.getHot().map(post => post.subreddit.display_name)
+      .then(sbrOfPost =>{
+        return this.subreddit = sbrOfPost
       })
   }
   // methods: {
@@ -41,5 +55,9 @@ li{
   text-align: left !important;
   padding: 5px !important;
   margin-left:10px;
+}
+span{
+  margin-left:20px;
+  font-weight: bold;
 }
 </style>
